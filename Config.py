c2c5b6262cc28e0eeae212c39c6b293445279b57
mzbotz26 <mrsam2395@gmail.com>
2025-11-24 22:53:10 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,8 +1,8 @@
 # WARNING: अपनी वास्तविक जानकारी से बदलें।
 # आप अपना बॉट टोकन @BotFather से प्राप्त कर सकते हैं।
-BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN_HERE"
+BOT_TOKEN = "7771472205:AAENcUwaWSrT_zx2W8agFA_BS5auhOn7pN4"
 
 # WARNING: एडमिन की Telegram User ID से बदलें।
 # यह ID वह व्यक्ति है जिसे 'ऑनलाइन आने' के रूप में सूचित किया जाएगा।
 # आप अपनी User ID खोजने के लिए @userinfobot का उपयोग कर सकते हैं।
-ADMIN_ID = 1234567890 
+ADMIN_ID = 6139607609
